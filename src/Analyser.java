44afac60907cbac9a11d95d17f2d4c54fb5e39b2
T De Haro <Tochitos2@gmail.com>
2020-05-23 23:40:43 +0100

    Fixed file reading.

--- src/Analyser.java
+++ src/Analyser.java
@@ -1,29 +1,48 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Analyser {
-    private List<String> commonWords;
+    private ArrayList<String> commonWords;
     private String commonWordsPath = "CommonWords.txt";
     private HashMap<String, Integer> words;
     private LinkedHashMap<String, Integer> sortedWords;
+    private int wordCount;
 
-    public List<String> getCommonWords(){
+    public Analyser(){
+        commonWords = new ArrayList<String>();
+        words = new HashMap<String, Integer>();
+        sortedWords = new LinkedHashMap<String, Integer>();
+
+        LoadCommonWords();
+    }
+
+    public void LoadCommonWords(){
 
         Scanner scanner = new Scanner(commonWordsPath);
 
         while(scanner.hasNextLine()){
             commonWords.add(scanner.nextLine());
         }
-
-        return commonWords;
     }
 
-    public void addFile(String path){
+    public void addFile(String path) {
 
-        Scanner scanner = new Scanner(path);
+        File file = new File(path);
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
         while(scanner.hasNext()){
-            String word = scanner.nextLine().replaceAll("[^a-zA-Z'-]", "");
-            words.merge(word, 1, Integer::sum);
+            String word = scanner.next().replaceAll("[^a-zA-Z'-]", "");
+            if(!commonWords.contains(word)) {
+                words.merge(word, 1, Integer::sum);
+                wordCount++;
+            }
         }
     }
 
@@ -36,6 +55,10 @@ public class Analyser {
                         (oldValue, newValue) -> oldValue, LinkedHashMap::new));
 
         return sortedWords;
+    }
+
+    public int getWordCount(){
+        return wordCount;
     }
 
 }
