76cc2cb40d9dde8dabde1157ebffa1728868f1c5
T De Haro <Tochitos2@gmail.com>
2020-05-28 14:23:57 +0100

    Added dialogue filtering capabilities to the analyser.

--- src/Analyser.java
+++ src/Analyser.java
@@ -5,15 +5,20 @@ import java.util.stream.Collectors;
 
 public class Analyser {
     private ArrayList<String> commonWords;
-    private String commonWordsPath = "src/CommonWords.txt";
+    private String commonWordsPath;
     private HashMap<String, Integer> words;
     private LinkedHashMap<String, Integer> sortedWords;
     private int wordCount;
+    private boolean inSpeech;
+    private Restriction restriction;
 
     public Analyser(){
         commonWords = new ArrayList<String>();
         words = new HashMap<String, Integer>();
         sortedWords = new LinkedHashMap<String, Integer>();
+        commonWordsPath = "src/CommonWords.txt";
+        inSpeech = false;
+        restriction = Restriction.TEXT;
 
         LoadCommonWords();
     }
@@ -45,13 +50,37 @@ public class Analyser {
         }
 
         while(scanner.hasNext()){
-            String word = capitalise(scanner.next().replaceAll("[^a-zA-Z'-]", ""));
+            String word = capitalise(scanner.next());
 
+            // If restrictions are applied words must be parsed for speech marks.
+            if(restriction == Restriction.DIALOGUE || restriction == Restriction.NARRATION) {
+                StringBuilder newWord = new StringBuilder();
+                for (int i = 0; i < word.length(); i++) {
+                    if (word.charAt(i) == '\"') {
+                        inSpeech = !inSpeech; //Flip whether in dialogue on speech mark.
+                    }
+                    // Builds the word according to whether the analyser is set to count dialogue or vice versa.
+                    if (restriction == Restriction.DIALOGUE && inSpeech) {
+                        newWord.append(word.charAt(i));
+                    }else if(restriction == Restriction.NARRATION && !inSpeech){
+                        newWord.append(word.charAt(i));
+                    }
+                }
+                word = newWord.toString();
+            }
+            // Filter out most punctuation.
+            word.replaceAll("[^a-zA-Z'-]", "");
             // If not a common word and contains at least 1 word character, then add.
             if(!commonWords.contains(word) && word.matches(".*\\w+.*")) {
                 words.merge(word, 1, Integer::sum);
                 wordCount++;
             }
+        }
+        // If the document ends with a single open speech mark there may be missing speech marks
+        // in the text, which would throw off the tracking of dialogue.
+        if(restriction != Restriction.TEXT && inSpeech){
+            System.out.println("Document ends with a trailing speech mark. Possible punctuation error in text." +
+                    "\nResults may be inaccurate.");
         }
     }
 
@@ -83,6 +112,8 @@ public class Analyser {
     public int getWordCount(){
         return wordCount;
     }
+
+    public void setRestriction(Restriction restriction) { this.restriction = restriction; }
 
 }
 
---
+++ src/Restriction.java
@@ -0,0 +1,3 @@
+public enum Restriction {
+    DIALOGUE, NARRATION, TEXT
+}
