2dde4410d60061375f11e1174a0ab5ec5af5cd45
T De Haro <Tochitos2@gmail.com>
2020-06-01 18:11:46 +0100

    Added JTable generation.

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,17 +1,28 @@
+import com.sun.xml.internal.ws.util.QNameMap;
+
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 import java.awt.*;
-import java.io.File;
-import java.io.IOException;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class MainWindow extends JFrame {
 
     private Container contentPane;
+    private JPanel filePanel, filterPanel, actionPanel, resultsPanel;
     private JButton analyseBtn, addFileBtn, addBlacklistBtn;
+    private JTable table1;
+    private JCheckBox checkBox1;
+    private JComboBox comboBox1;
     private JCheckBox excludeCommonChkBx;
     private JLabel excludeCommonLbl;
+
     private int fileCount;
     private Desktop desktop = null;
     private Analyser analyser = new Analyser();
@@ -21,22 +32,76 @@ public class MainWindow extends JFrame {
     }
 
     public MainWindow(){
-        super("Wordcount Ranker");
+        // Container initialisation
+        super("Word Ranker");
         contentPane = getContentPane();
-        contentPane.setLayout(new GridLayout(2,1));
-        contentPane.setPreferredSize(new Dimension(300,300));
+        contentPane.setLayout(new GridLayout(3,2));
+        contentPane.setPreferredSize(new Dimension(400,250));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // File panel initialisation
+        filePanel = new JPanel(new GridBagLayout());
+        Border fileBorder = BorderFactory.createTitledBorder("Files");
+        filePanel.setBorder(fileBorder);
         addFileBtn = new JButton("Add File");
         addFileBtn.addActionListener(e-> this.addFile());
-        contentPane.add(addFileBtn);
+        filePanel.add(addFileBtn);
 
+
+        // Filter panel initialisation
+        filterPanel = new JPanel(new GridBagLayout());
+        Border filterBorder = BorderFactory.createTitledBorder("Filters");
+        filterPanel.setBorder(filterBorder);
+
+        // Action panel initialisation
+        actionPanel = new JPanel(new GridBagLayout());
         analyseBtn = new JButton("Analyse Text");
         analyseBtn.addActionListener(e-> this.showResults());
         contentPane.add(analyseBtn);
 
+        // Results panel initialisation
+        resultsPanel = new JPanel(new GridLayout());
         pack();
-        setVisible(true);
+        setVisible(false);
+    }
+
+    private JScrollPane createResultsTable(LinkedHashMap<String, Integer> data) {
+
+        // Create table data model.
+        String[] columnNames = { "Rank", "Word", "Count" };
+        TableModel dataModel = new DefaultTableModel() {
+            public String getColumnName(int col){ return columnNames[col]; }
+            public int getRowCount() { return data.size(); }
+            public int getColumnCount() { return columnNames.length; }
+            public Object getValueAt(int rowIndex, int columnIndex) {
+                if(columnIndex == 0){ return rowIndex; }
+                else if(columnIndex == 1){
+                    Set<Map.Entry<String, Integer>> mapSet = data.entrySet();
+                    Map.Entry<String, Integer> element = (Map.Entry<String, Integer>) mapSet.toArray()[rowIndex];
+                    return element.getKey();
+                }
+                else{
+                    Set<Map.Entry<String, Integer>> mapSet = data.entrySet();
+                    Map.Entry<String, Integer> element = (Map.Entry<String, Integer>) mapSet.toArray()[rowIndex];
+                    return element.getValue();
+                }
+            }
+        };
+
+        JTable table = new JTable(dataModel);
+        DefaultTableModel model = (DefaultTableModel) table.getModel();
+
+        // Populate data model
+        int rank = 1;
+        Set<String> keys = data.keySet();
+        for(String key: keys){
+            Object rowData[] = {rank, key, data.get(key)};
+            model.addRow(rowData);
+            rank++;
+        }
+
+        JScrollPane scrollPane = new JScrollPane(table);
+        return scrollPane;
     }
 
 
@@ -48,20 +113,16 @@ public class MainWindow extends JFrame {
         int returnVal = chooser.showOpenDialog(null);
         if(returnVal == JFileChooser.APPROVE_OPTION) {
             analyser.addFile(chooser.getSelectedFile().getAbsolutePath());
+            fileCount++;
         }
     }
 
     private void showResults(){
         LinkedHashMap<String, Integer> results = analyser.getSortedList();
-        int rank = 0;
-        for(Map.Entry<String, Integer> entry : results.entrySet()){
-            String key = entry.getKey();
-            int value = entry.getValue();
-            rank++;
+        JScrollPane scrollPane = createResultsTable(results);
 
-            System.out.println(rank + ". " + key + ": " + value);
-            if(rank >= 200){ return; }
-        }
+        resultsPanel.add(scrollPane);
+        resultsPanel.setVisible(true);
     }
 
 }
